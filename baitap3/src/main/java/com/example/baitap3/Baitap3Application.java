26c12d371356f19d9cc588fbb13e472c8ea3ea78
duc <duc53044@donga.edu.vn>
2024-09-19 18:36:09 +0700

    addAPI(test-h2)

---
+++ baitap3/src/main/java/com/example/baitap3/Baitap3Application.java
@@ -0,0 +1,13 @@
+package com.example.baitap3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baitap3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Baitap3Application.class, args);
+	}
+
+}
