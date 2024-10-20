a87476404ac18a77d5d804a7aa2c95ced2034347
duc <duc53044@donga.edu.vn>
2024-10-20 11:27:16 +0700

    done-companyApi

---
+++ Restapi_method/src/main/java/Restapi_method/controller/CompanyController.java
@@ -0,0 +1,47 @@
+package Restapi_method.controller;
+
+import Restapi_method.entity.Company;
+import Restapi_method.service.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/company")
+public class CompanyController {
+
+    @Autowired
+    private CompanyService companyService;
+
+    @GetMapping
+    private List<Company> getAllCompany() {
+        return companyService.getAllCompanies();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Company> getCompanyById(@PathVariable("id") Long id){
+        Company company = companyService.getCompanyById(id);
+        return new ResponseEntity<>(company , HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addCompany(@RequestBody Company company){
+        companyService.addCompany(company);
+        return ResponseEntity.ok("Đăng ký doanh nghiêp thành công");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateCompany(@PathVariable("id") Long id, @RequestBody Company company){
+        companyService.updatedCompany(id,company);
+        return ResponseEntity.ok("Cập nhật thành công");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCompany(@PathVariable("id") Long id){
+        companyService.deleteCompanyById(id);
+        return ResponseEntity.ok("Xoá thành công");
+    }
+}
--- Restapi_method/src/main/java/Restapi_method/service/CompanyService.java
+++ Restapi_method/src/main/java/Restapi_method/service/CompanyService.java
@@ -1,13 +1,43 @@
 package Restapi_method.service;
 
+import Restapi_method.entity.Company;
 import Restapi_method.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CompanyService {
     @Autowired
     private CompanyRepository companyRepository;
 
+    public List<Company> getAllCompanies(){
+        return companyRepository.findAll();
+    }
 
+    public Company getCompanyById(Long id) {
+        return companyRepository.findById(id).orElse(null);
+    }
+
+    public Company addCompany(Company company){
+        return companyRepository.save(company);
+    }
+
+    public Optional<Company> updatedCompany(Long id , Company updatedCompany){
+        Optional<Company> updateCompanyById = companyRepository.findById(id);
+        if (updateCompanyById.isPresent()) {
+            Company company = updateCompanyById.get();
+            company.setCompanyName(updatedCompany.getCompanyName());
+            companyRepository.save(company);
+            return Optional.of(company);
+        }
+        return Optional.empty();
+    }
+
+    public boolean deleteCompanyById(Long id){
+        companyRepository.deleteById(id);
+        return true;
+    }
 }
